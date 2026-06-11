e4c015d241ab2586c1ce1e91d3bf8f6cc0bdfe9a
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-06-11 12:14:13 +0530

    add uu in admin aboutsettings component

--- src/components/admin/AboutSettings.jsx
+++ src/components/admin/AboutSettings.jsx
@@ -67,7 +67,7 @@ export default function AboutSettings({ triggerToast }) {
           if (data.list) setPointsList(data.list);
         }
 
-        // Partners
+        // Partnersuu
         const partnersDoc = await getDoc(doc(db, "about", "partners"));
         if (partnersDoc.exists()) {
           const data = partnersDoc.data();
