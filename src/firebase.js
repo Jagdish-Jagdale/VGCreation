935ce8a2230160341faed1e7b5aa74e8d5e7f473
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-06-30 14:15:26 +0530

    remove ax in firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Primary Firebase Appax
+// Initialize Primary Firebase App
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
